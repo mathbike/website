40386c012a2c121480235952e19a5f4103d2ae8e
mathbike <mathbike8@gmail.com>
2022-08-28 06:03:13 -0400

    d

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
