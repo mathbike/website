5f56ccf29f63a04567a3d611f57408622d0fd771
mathbike <mathbike8@gmail.com>
2022-08-28 06:07:32 -0400

    d

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
 // function to toggle between light and dark theme
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
@@ -6,3 +11,13 @@ function toggleTheme() {
         setTheme('theme-dark');
     }
 }
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+      document.getElementById('slider').checked = true;
+    }
+})();
